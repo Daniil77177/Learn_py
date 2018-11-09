032426f6e64fa6e7f7374ddc9ab8e2a141e72530
Daniil77177 <Daniil200277177@gmail.com>
2018-11-09 07:38:51 +0300

    V_5.1 See changelog (Some changes in example)

---
+++ 0.2.Random_Module.py
@@ -0,0 +1,14 @@
+import random
+
+for i in range(5):
+	#We create range. 0 - 4 (include 4)
+	#Мы создали range (участок). 0 - 4 (4 включительно)
+	
+	
+	a = random.randint(0, 5) #There will be displayed random numbers
+	#From 0 to 5
+	#Сдесь будут показаны рандомные (случайные) числа
+	#От 0 до 5
+	
+	print (a)
+	
