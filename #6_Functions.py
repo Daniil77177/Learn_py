555945e61fee406b95317d65a89c2d3380471799
Daniil77177 <Daniil200277177@gmail.com>
2018-11-09 12:52:42 +0300

    V_5.1 See changelog (Add 0.2.Random_Modules, #5_Cycles, some changes

---
+++ #6_Functions.py
@@ -0,0 +1,12 @@
+#When you must use one piece of your code offen
+#You write a function
+#def name_func (requirements)   (syntax)
+#Когда вам нужно использовать один кусок вашего кода часто,
+#Вы пишете функцию
+#def name_func (аргументы)   (синтаксис)
+#Example 1 || Пример 1
+def printsome (x):
+	print(x)
+
+a = input("Write something: ")
+printsome(a)
---
+++ 0.3.Programm_for_learning_math(quad).py
@@ -0,0 +1,15 @@
+import random
+randomintegger = randint(1, 10) 
+i = 1
+if randomintegger == 1:
+	while i not = 2
+		x = input("Введите квадратный корень 11: ")
+		if x == 121:
+			print("Правильно/Success")
+			i =2
+			break
+		else:
+			print("Try again")
+		
+	
+
