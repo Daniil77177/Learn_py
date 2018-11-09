6353cdb889025d71f117be1ac6b3f2179e9dc8f7
Daniil77177 <Daniil200277177@gmail.com>
2018-11-09 15:21:11 +0200

    Some changes

---
+++ #4_Cycles.py
@@ -0,0 +1,14 @@
+#We have 2 types of cycle in Python for and while 
+#Есть 2 типа циклов в Python for (для) и while (пока)
+
+#Let's begin with while
+#Начнём с while
+
+a = 0
+while a < 100:
+    print(a)
+    a += 1
+    #Code will print 100 times, (because 100 not smaller than 100)
+    #Код выведется 100 раз, (потому что 100 не меньше чем 100)
+
+
