8ec6dc4e605869eca6dcbcb7bd923b0629b69f70
Daniil77177 <Daniil200277177@gmail.com>
2018-11-13 22:37:28 +0200

    Some changes (pc)

--- 0.3.Programm_for_learning_math(quad).py
+++ 0.3.Programm_for_learning_math(quad).py
@@ -2,12 +2,13 @@ import random
 randomintegger = randint(1, 10) 
 i = 1
 if randomintegger == 1:
-	while i not = 2
+
+
+	while i != 2
 		x = input("Введите квадратный корень 11: ")
 		if x == 121:
 			print("Правильно/Success")
-			i =2
-			break
+			i = 2
 		else:
 			print("Try again")
 		
